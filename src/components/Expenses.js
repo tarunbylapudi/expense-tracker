c3fb60a65765d19a51f39d60ad5efa6ebcd6b942
Tarun Bylapudi <bylasoft@gmail.com>
2023-05-29 23:13:38 +0530

    basic setup and components

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.items[0]} />
+      <ExpenseItem expense={props.items[1]} />
+      <ExpenseItem expense={props.items[2]} />
+      <ExpenseItem expense={props.items[3]} />
+    </Card>
+  );
+};
+
+export default Expenses;
